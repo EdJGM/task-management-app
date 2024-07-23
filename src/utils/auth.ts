76ca9a5f3b4472cc6586d3c075ea4b0a6d9c67ef
Edgar <ejgallegos@espe.edu.ec>
2024-07-23 00:48:43 -0500

    chore: Remove unused auth utility file

--- src/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-// utils/auth.ts
-export const isAuthenticated = () => {
-    if (typeof window !== 'undefined') {
-        return sessionStorage.getItem('user') === 'true';
-    }
-    return false;
-};
